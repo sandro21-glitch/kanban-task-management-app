9cca41c6bdbc27245b311f2ba8d84a1da329d787
Sandro <vipsandro11@gmail.com>
2023-10-27 23:10:29 +0400

    feat: Enhance Button component with optional onClick prop

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -2,11 +2,15 @@ type ButtonTypes = {
   img: string;
   text: string;
   isActive: boolean;
+  onClick?: () => void;
 };
-const Button = ({ img, text, isActive }: ButtonTypes) => {
+const Button = ({ img, text, isActive, onClick }: ButtonTypes) => {
   return (
     <button
-      className={`${isActive ? 'bg-mediumPurple text-white' : ''} capitalize font-bold text-lightGray flex items-center
+      onClick={onClick}
+      className={`${
+        isActive ? "bg-mediumPurple text-white" : ""
+      } capitalize font-bold text-lightGray flex items-center
      gap-3 py-3 px-6 w-[90%] cursor-pointer hover:bg-hoverGray
       hover:text-mediumPurple rounded-r-3xl transition-all ease-in duration-150`}
     >
