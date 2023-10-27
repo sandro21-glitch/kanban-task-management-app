d16476245008ca527267554736f582cde7b741b1
Sandro <vipsandro11@gmail.com>
2023-10-27 23:11:42 +0400

    feat: Integrate Navbar and CreateNewBoard components, manage sidebar state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import Home from "./pages/Home";
 import Sidebar from "./ui/Sidebar";
+import Navbar from "./ui/Navbar";
+import CreateNewBoard from "./features/forms/CreateNewBoard";
 
 function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(true);
+
   return (
     <main className="flex">
-      <Sidebar />
-      <Home />
+      <Navbar />
+      <Sidebar
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <Home isSidebarOpen={isSidebarOpen} />
+      <CreateNewBoard />
     </main>
   );
 }
