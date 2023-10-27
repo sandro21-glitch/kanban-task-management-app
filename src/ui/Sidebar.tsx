8d06cc29c12ff329ce472d83e4da8249b7d8035e
Sandro <vipsandro11@gmail.com>
2023-10-27 23:09:43 +0400

    refactor: Update Sidebar component to conditionally render based on isSidebarOpen

--- src/ui/Sidebar.tsx
+++ src/ui/Sidebar.tsx
@@ -1,18 +1,20 @@
+import OpenSidebar from "./OpenSidebar";
 import SidebarControl from "./SidebarControl";
 import SidebarList from "./SidebarList";
-import logo from "/assets/logo.svg";
-const Sidebar = () => {
+
+type SidebarTypes = {
+  isSidebarOpen: boolean;
+  setIsSidebarOpen: (open: boolean) => void;
+};
+
+const Sidebar = ({ isSidebarOpen, setIsSidebarOpen }: SidebarTypes) => {
+  if (!isSidebarOpen)
+    return <OpenSidebar setIsSidebarOpen={setIsSidebarOpen} />;
   return (
-    <aside className="min-w-[261px] fixed left-0 top-0 bottom-0 bg-white">
+    <aside className="min-w-[261px] fixed left-0 top-[5rem] bottom-0">
       <div className="flex justify-between flex-col h-full">
-        <div>
-          <div className="flex items-center gap-5 mb-5 p-7">
-            <img src={logo} alt="logo" />
-            <h2 className="font-bold">kanban</h2>
-          </div>
-          <SidebarList />
-        </div>
-        <SidebarControl />
+        <SidebarList />
+        <SidebarControl setIsSidebarOpen={setIsSidebarOpen} />
       </div>
     </aside>
   );
