6f0009f9cdf1bd5b065e03dab918adf224d0de7f
Sandro <vipsandro11@gmail.com>
2023-10-30 14:26:19 +0400

    style: Removed unused code in Tasks component

--- src/features/tasks/Tasks.tsx
+++ src/features/tasks/Tasks.tsx
@@ -1,17 +1,20 @@
 // import { useTasksContext } from "../context/TasksContext";
 
+import SingleTaskPage from "./SingleTaskPage";
+
 type Sidebar = {
   isSidebarOpen: boolean;
 };
 const Tasks = ({ isSidebarOpen }: Sidebar) => {
-  // const { state } = useTasksContext();
-  // const { allTask } = state;
+
   return (
     <article
       className={`bg-boardColor ${
         isSidebarOpen ? "ml-[261px]" : "ml-[0px]"
       } py-[104px] px-5 h-screen`}
-    >test</article>
+    >
+      <SingleTaskPage />
+    </article>
   );
 };
 export default Tasks;
