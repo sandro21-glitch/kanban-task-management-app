6607b9af6506ea77f1b629943cc83f70106d2ef4
Sandro <vipsandro11@gmail.com>
2023-10-30 14:30:17 +0400

    refactor: Updated 'Sidebar' component to accept 'setOpenBoardForm' prop

--- src/ui/Sidebar.tsx
+++ src/ui/Sidebar.tsx
@@ -5,15 +5,20 @@ import SidebarList from "./SidebarList";
 type SidebarTypes = {
   isSidebarOpen: boolean;
   setIsSidebarOpen: (open: boolean) => void;
+  setOpenBoardForm: (open: boolean) => void;
 };
 
-const Sidebar = ({ isSidebarOpen, setIsSidebarOpen }: SidebarTypes) => {
+const Sidebar = ({
+  isSidebarOpen,
+  setIsSidebarOpen,
+  setOpenBoardForm,
+}: SidebarTypes) => {
   if (!isSidebarOpen)
     return <OpenSidebar setIsSidebarOpen={setIsSidebarOpen} />;
   return (
     <aside className="min-w-[261px] fixed left-0 top-[5rem] bottom-0">
       <div className="flex justify-between flex-col h-full">
-        <SidebarList />
+        <SidebarList setOpenBoardForm={setOpenBoardForm} />
         <SidebarControl setIsSidebarOpen={setIsSidebarOpen} />
       </div>
     </aside>
