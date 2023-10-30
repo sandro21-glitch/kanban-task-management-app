b5f9dd648c83144f4a7b410b7dd5ae7b431e18aa
Sandro <vipsandro11@gmail.com>
2023-10-30 14:28:56 +0400

    refactor: Updated 'Actions' type

--- src/types/Actions.ts
+++ src/types/Actions.ts
@@ -1,4 +1,17 @@
-export type Actions = {
-    type: "TOGGLE_LAYOUT";
-    payload: string;
-  };
+// import { StateTypes } from "./StateTypes";
+
+import { BoardTypes } from "./StateTypes";
+
+export type Actions =
+  | {
+      type: "TOGGLE_LAYOUT";
+      payload: string;
+    }
+  | {
+      type: "ADD_NEW_BOARD";
+      payload: BoardTypes;
+    }
+  | {
+      type: "SET_ACTIVE_BOARD";
+      payload: number;
+    };
