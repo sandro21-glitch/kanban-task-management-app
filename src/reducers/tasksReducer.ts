e7a1aaa7a1fd7758982a1e32d6761f0203bb700d
Sandro <vipsandro11@gmail.com>
2023-10-30 14:28:37 +0400

    feat: Expanded tasksReducer to handle 'ADD_NEW_BOARD' and 'SET_ACTIVE_BOARD' actions

--- src/reducers/tasksReducer.ts
+++ src/reducers/tasksReducer.ts
@@ -1,10 +1,16 @@
 import { Actions } from "../types/Actions";
 import { StateTypes } from "../types/StateTypes";
 
-
 export const tasksReducer = (state: StateTypes, action: Actions) => {
   if (action.type === "TOGGLE_LAYOUT") {
-    console.log("testing");
+    // ... your logic here ...
+  }
+  if (action.type === "ADD_NEW_BOARD") {
+    const updateBoard = action.payload;
+    return { ...state, board: [...state.board, updateBoard] } as StateTypes;
+  }
+  if (action.type === "SET_ACTIVE_BOARD") {
+    return { ...state, activeBoard: action.payload };
   }
   return state;
 };
