0447ebd3df27ccb227eef8298f278eb4fe6c9aa9
Sandro <vipsandro11@gmail.com>
2023-10-30 14:25:33 +0400

    refactor: Updated TasksContext for managing boards and active board index

--- src/context/TasksContext.tsx
+++ src/context/TasksContext.tsx
@@ -1,35 +1,37 @@
 import { createContext, useContext, ReactNode, useReducer } from "react";
 import { tasksReducer } from "../reducers/tasksReducer";
 import { Actions } from "../types/Actions";
-import { StateTypes } from "../types/StateTypes";
+import { BoardTypes, StateTypes } from "../types/StateTypes";
 
 type TasksContextType = {
   state: StateTypes;
   dispatch: React.Dispatch<Actions>;
+  addNewBoard: (board: BoardTypes) => void;
+  setActiveBoardIndex: (index: number) => void;
 };
 
 const TasksProvider = createContext<TasksContextType | null>(null);
 
 const initialState = {
-  allTask: [
-    {
-      boardName: "test",
-      todos: [
-        {
-          todoName: "test",
-          status: "todo",
-        },
-      ],
-      subtasks: [{ task: "test", completed: false }],
-    },
-  ],
+  board: [],
+  activeBoard: null,
 };
 
 const TasksContext: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [state, dispatch] = useReducer(tasksReducer, initialState);
-  console.log(state);
+
+  const addNewBoard = (boardData: BoardTypes): void => {
+    dispatch({ type: "ADD_NEW_BOARD", payload: boardData });
+  };
+
+  const setActiveBoardIndex = (index: number): void => {
+    dispatch({ type: "SET_ACTIVE_BOARD", payload: index });
+  };
+
   return (
-    <TasksProvider.Provider value={{ state, dispatch }}>
+    <TasksProvider.Provider
+      value={{ state, dispatch, addNewBoard, setActiveBoardIndex }}
+    >
       {children}
     </TasksProvider.Provider>
   );
