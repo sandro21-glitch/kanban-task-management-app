f09b3538aac5ce01295f38a7bed10b102de032d7
Sandro <vipsandro11@gmail.com>
2023-11-01 23:22:52 +0400

    refactor: Extended App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import Home from "./pages/Home";
 import Sidebar from "./ui/Sidebar";
 import Navbar from "./ui/Navbar";
 import CreateNewBoard from "./features/forms/CreateNewBoard";
+import EditBoard from "./features/tasks/EditBoard";
 
 function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(true);
   const [openBoardForm, setOpenBoardForm] = useState<boolean>(false);
+  const [addNewColumnForm, setAddNewColumnForm] = useState(false);
   return (
     <main>
       <Navbar />
@@ -15,11 +17,15 @@ function App() {
         setIsSidebarOpen={setIsSidebarOpen}
         setOpenBoardForm={setOpenBoardForm}
       />
-      <Home isSidebarOpen={isSidebarOpen} />
+      <Home
+        isSidebarOpen={isSidebarOpen}
+        setAddNewColumnForm={setAddNewColumnForm}
+      />
       <CreateNewBoard
         openBoardForm={openBoardForm}
         setOpenBoardForm={setOpenBoardForm}
       />
+      <EditBoard addNewColumnForm={addNewColumnForm} />
     </main>
   );
 }
