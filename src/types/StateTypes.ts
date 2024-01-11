196120a792f5bed634ef4b2abbd7cba75b72385f
Sandro <vipsandro11@gmail.com>
2024-01-11 23:12:46 +0400

    refactor: Update state types to include selectedBoard in TasksContext

--- src/types/StateTypes.ts
+++ src/types/StateTypes.ts
@@ -13,8 +13,8 @@ export type BoardTypes = {
   boardName: string;
   todos: TodoType[];
 };
-
 export type StateTypes = {
   board: BoardTypes[];
   activeBoard: number | null;
+  selectedBoard: BoardTypes[];
 };
