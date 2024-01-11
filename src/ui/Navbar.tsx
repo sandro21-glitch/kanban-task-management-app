f2c5fd7f705f1b24b3a4a084aafe5096322b6529
Sandro <vipsandro11@gmail.com>
2024-01-11 23:11:44 +0400

    refactor: pass setAddNewTask prop to TasksHeader from Navbar

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -1,6 +1,9 @@
 import TasksHeader from "./TasksHeader";
 import logo from "/assets/logo.svg";
-const Navbar = () => {
+type NavbarTypes = {
+  setAddNewTask: (bool: boolean) => void;
+};
+const Navbar = ({ setAddNewTask }: NavbarTypes) => {
   return (
     <header className="fixed left-0 top-0 right-0 bg-white z-10">
       <nav className="flex items-center p-5">
@@ -8,7 +11,7 @@ const Navbar = () => {
           <img src={logo} alt="logo" />
           <h2 className="font-bold">kanban</h2>
         </div>
-        <TasksHeader />
+        <TasksHeader setAddNewTask={setAddNewTask} />
       </nav>
     </header>
   );
