59d52fa85ff5de3182484b9f55595bd673595359
Sandro <vipsandro11@gmail.com>
2024-01-11 23:11:24 +0400

    refactor: update TasksHeader with dynamic task addition

--- src/ui/TasksHeader.tsx
+++ src/ui/TasksHeader.tsx
@@ -1,15 +1,21 @@
 import { useTasksContext } from "../context/TasksContext";
 import iconVertical from "/assets/icon-vertical-ellipsis.svg";
-const TasksHeader = () => {
+
+type TasksHeaderTypes = {
+  setAddNewTask: (bool: boolean) => void;
+};
+
+const TasksHeader = ({ setAddNewTask }: TasksHeaderTypes) => {
   const { state } = useTasksContext();
   const { board, activeBoard } = state;
 
-  if(activeBoard == null) return
+  if (activeBoard == null) return;
   return (
     <div className="w-full flex items-center justify-between">
       <h4 className="font-bold ml-12">{board[activeBoard].boardName}</h4>
       <div className="flex items-center gap-5 mr-5">
         <button
+          onClick={() => setAddNewTask(true)}
           className="capitalize bg-mediumPurple text-white p-[10px]
          rounded-3xl font-bold hover:opacity-75 transition-opacity ease-in duration-150"
         >
