a086dfa6dfb8332f688c799e761e57a72dba36b5
Sandro <vipsandro11@gmail.com>
2024-01-13 21:50:59 +0400

    feat: Introduce BoardControls component

---
+++ src/ui/BoardControls.tsx
@@ -0,0 +1,11 @@
+import iconVert from "/assets/icon-vertical-ellipsis.svg";
+
+const BoardControls = () => {
+  return (
+    <button>
+      <img src={iconVert} alt="icon vertical ellipsis" />
+    </button>
+  );
+};
+
+export default BoardControls;
