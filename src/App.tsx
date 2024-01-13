322ad4494de2472a7d0789694757ce316686b1e7
Sandro <vipsandro11@gmail.com>
2024-01-13 21:48:15 +0400

    feat: Integrate Navbar and Home components into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import Home from "./pages/Home";
+import Navbar from "./ui/Navbar";
+
 function App() {
-  return <main></main>;
+  return (
+    <main>
+      <Navbar />
+      <Home />
+    </main>
+  );
 }
 
 export default App;
