313761a8a118928ef3b69d7d6a74852e78abf57c
Sandro <vipsandro11@gmail.com>
2024-01-13 21:49:01 +0400

    feat: Create AddNewTaskBtn component

---
+++ src/ui/AddNewTaskBtn.tsx
@@ -0,0 +1,9 @@
+const AddNewTaskBtn = () => {
+  return (
+    <button className="bg-mediumPurple w-[164px] h-[3em] rounded-full text-white font-bold normal-case">
+      + Add new task
+    </button>
+  );
+};
+
+export default AddNewTaskBtn;
