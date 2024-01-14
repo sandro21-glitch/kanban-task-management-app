39d1271b99889c9b143364b25d8b583429a6593b
Sandro <vipsandro11@gmail.com>
2024-01-14 23:00:00 +0400

    initial commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,11 @@ import Sidebar from "../ui/Sidebar";
 
 const Home = () => {
   return (
-    <section className="flex">
+    <section className="flex w-full min-h-full relative overflow-hidden">
       <Sidebar />
+      <div className="w-full h-[90vh]  p-5 ">
+        <h1>1</h1>
+      </div>
     </section>
   );
 };
