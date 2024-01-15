e941433992c4a6b863a9e81d0a178b17d2fbb7d7
Sandro <vipsandro11@gmail.com>
2024-01-15 22:59:12 +0400

    refactor: integrate dark mode support in AddNewColumnBtn

--- src/features/board/AddNewColumnBtn.tsx
+++ src/features/board/AddNewColumnBtn.tsx
@@ -1,8 +1,13 @@
+import { useAppSelector } from "../../hooks/reduxHooks";
+
 const AddNewColumnBtn = () => {
+  const darkMode = useAppSelector((store) => store.theme.darkMode);
   return (
     <button
-      className="min-w-[17.5em] bg-[#e9effa8e]
-     flex justify-center items-center hover:scale-[1.02] transition-all ease-in duration-150"
+      className={`min-w-[17.5em] 
+      flex justify-center items-center
+      ${darkMode ? "bg-gradientGray" : "bg-[#e9effa8e]"}
+      hover:scale-[1.02] transition-all ease-in duration-150 rounded-lg`}
     >
       <span className="text-[1.5rem] font-bold text-lightGray">
         + New Column
