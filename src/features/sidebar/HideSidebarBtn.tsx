499c392aad0d2a35a3e1ed15531ea75fdfbbcba3
Sandro <vipsandro11@gmail.com>
2024-01-15 23:00:03 +0400

    style: update HideSidebarBtn to dynamically adjust styles based on dark mode

--- src/features/sidebar/HideSidebarBtn.tsx
+++ src/features/sidebar/HideSidebarBtn.tsx
@@ -1,15 +1,19 @@
 import { setSidebar } from "../popups/popupSlice";
-import { useAppDispatch } from "../../hooks/reduxHooks";
+import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
 import hideSidebar from "/assets/hide-sidebar.svg";
 
 const HideSidebarBtn = () => {
   const dispatch = useAppDispatch();
-
+  const darkMode = useAppSelector((store) => store.theme.darkMode);
   return (
     <button
       onClick={() => dispatch(setSidebar())}
-      className="hover:bg-hoverGray w-[90%] pl-5 flex
-   items-center justify-start gap-5 py-[1rem] font-bold text-lightGray hover:text-mediumPurple rounded-r-full transition-all ease-in duration-150 cursor-pointer"
+      className={`${
+        darkMode ? "hover:bg-white" : "hover:bg-hoverGray"
+      } w-[90%] pl-5 flex
+      items-center justify-start gap-5 py-[1rem] font-bold
+       text-lightGray hover:text-mediumPurple
+        rounded-r-full transition-all ease-in duration-150 cursor-pointer`}
     >
       <img src={hideSidebar} alt="hide sidebar" />
       Hide Sidebar
