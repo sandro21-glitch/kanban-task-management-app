580078f4a827999af2fdc190f3b6af27bcc8233f
Sandro <vipsandro11@gmail.com>
2024-01-15 17:23:27 +0400

    feat: create AddNewColumnBtn component

---
+++ src/features/board/AddNewColumnBtn.tsx
@@ -0,0 +1,14 @@
+const AddNewColumnBtn = () => {
+  return (
+    <button
+      className="min-w-[17.5em] bg-[#e9effa8e]
+     flex justify-center items-center hover:scale-[1.02] transition-all ease-in duration-150"
+    >
+      <span className="text-[1.5rem] font-bold text-lightGray">
+        + New Column
+      </span>
+    </button>
+  );
+};
+
+export default AddNewColumnBtn;
