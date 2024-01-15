2acef6ff26dcf8ce114386ec515880cb0bf0dd03
Sandro <vipsandro11@gmail.com>
2024-01-15 21:03:47 +0400

    feat: Update SidebarBot component to use Redux state for dark mode and adjust styling based on the dark mode state

--- src/features/sidebar/SidebarBot.tsx
+++ src/features/sidebar/SidebarBot.tsx
@@ -1,17 +1,24 @@
 import lightMode from "/assets/light-mode.svg";
-import darkMode from "/assets/dark-mode.svg";
+import darkModeImage from "/assets/dark-mode.svg";
 import HideSidebarBtn from "./HideSidebarBtn";
+import { useAppSelector } from "../../hooks/reduxHooks";
 
 const SidebarBot = () => {
+  const darkMode = useAppSelector(
+    (store) => store.theme.darkMode
+  );
   return (
     <div>
-      <div className="flex items-center justify-center gap-5 mb-5 bg-boardColor py-[.9rem] max-w-[90%] rounded-lg">
+      <div
+        className={`flex items-center justify-center
+         gap-5 mb-5 ${darkMode ? 'bg-mediumDark' : "bg-hoverGray"} bg-boardColor py-[.9rem] max-w-[90%] mx-auto rounded-lg`}
+      >
         <img src={lightMode} alt="light mode" />
         <label className="relative inline-flex items-center cursor-pointer">
           <input type="checkbox" value="" className="sr-only peer" />
           <div className="w-11 h-6 bg-mediumPurple peer-focus:outline-none peer-focus:ring-4 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-mediumPurple"></div>
         </label>
-        <img src={darkMode} alt="dark mode" />
+        <img src={darkModeImage} alt="dark mode" />
       </div>
       <HideSidebarBtn />
     </div>
