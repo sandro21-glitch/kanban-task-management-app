6ecc273679b05972cd12a5242b54ada85d3db326
Sandro <vipsandro11@gmail.com>
2024-01-15 22:59:48 +0400

    style: update BoardColumns component to apply light gray text color

--- src/features/board/BoardColumns.tsx
+++ src/features/board/BoardColumns.tsx
@@ -1,7 +1,7 @@
 const BoardColumns = () => {
   return (
     <article>
-      <ul className="flex">
+      <ul className="flex text-lightGray">
         <li className="min-w-[17.5em]">Todo (1)</li>
         <li className="min-w-[17.5em]">IN PROGRESS (0)</li>
         <li className="min-w-[17.5em]">Done (2)</li>
