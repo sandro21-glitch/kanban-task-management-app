63eebef8981d758cc4827659265be203ed5d301c
Sandro <vipsandro11@gmail.com>
2024-01-15 22:59:32 +0400

    style: update Board component to dynamically apply background color based on dark mode

--- src/features/board/Board.tsx
+++ src/features/board/Board.tsx
@@ -1,9 +1,15 @@
+import { useAppSelector } from "../../hooks/reduxHooks";
 import AddNewColumnBtn from "./AddNewColumnBtn";
 import BoardColumns from "./BoardColumns";
 
 const Board = () => {
+  const darkMode = useAppSelector((store) => store.theme.darkMode);
   return (
-    <section className="w-full h-[90vh] p-5 flex gap-5 bg-boardColor">
+    <section
+      className={`w-full h-[90vh] p-5 flex gap-5 ${
+        darkMode ? "bg-boardColorDark" : "bg-boardColor"
+      }`}
+    >
       <BoardColumns />
       <AddNewColumnBtn />
     </section>
