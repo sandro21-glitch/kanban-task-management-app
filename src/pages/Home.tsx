d5ad64ddc84d35932aba9486a922aaa5c30fd9c2
Sandro <vipsandro11@gmail.com>
2024-01-15 17:22:38 +0400

    refactor: update Home component to render Board instead of a static content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,11 @@
+import Board from "../features/board/Board";
 import Sidebar from "../ui/Sidebar";
 
 const Home = () => {
   return (
     <section className="flex w-full min-h-full relative overflow-hidden">
       <Sidebar />
-      <div className="w-full h-[90vh]  p-5 ">
-        <h1>1</h1>
-      </div>
+      <Board />
     </section>
   );
 };
