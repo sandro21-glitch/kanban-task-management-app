b658e7645ec3ed996cc7443bd3cd3861ef8a64d7
Sandro <vipsandro11@gmail.com>
2024-01-15 17:23:05 +0400

    feat: add theme slice for managing dark mode state

---
+++ src/features/ThemeSwitcher/themeSwitch.ts
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+// import type { PayloadAction } from "@reduxjs/toolkit";
+// import type { RootState } from "../../store";
+
+// Define a type for the slice state
+interface ThemeState {
+  darkMode: boolean;
+}
+
+// Define the initial state using that type
+const initialState: ThemeState = {
+  darkMode: false,
+};
+
+export const themeSlice = createSlice({
+  name: "theme",
+  initialState,
+  reducers: {},
+});
+
+export const {} = themeSlice.actions;
+
+// Other code such as selectors can use the imported `RootState` type
+// export const selectCount = (state: RootState) => state.counter.value;
+
+export default themeSlice.reducer;
