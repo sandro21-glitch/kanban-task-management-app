67876fe29a5c24958d10f387555b8d42ca1f3442
Sandro <vipsandro11@gmail.com>
2024-01-15 19:57:43 +0400

    refactor: moving light/dark mode toggle and HideSidebarBtn to SidebarBot component

--- src/features/sidebar/Sidebar.tsx
+++ src/features/sidebar/Sidebar.tsx
@@ -1,9 +1,8 @@
 import SidebarTop from "./SidebarTop";
-import lightMode from "/assets/light-mode.svg";
-import darkMode from "/assets/dark-mode.svg";
-import HideSidebarBtn from "./HideSidebarBtn";
+
 import { useAppSelector } from "../../hooks/reduxHooks";
 import OpenSidebar from "./OpenSidebar";
+import SidebarBot from "./SidebarBot";
 const Sidebar = () => {
   const { isSidebarOpen } = useAppSelector((store) => store.popup);
   if (!isSidebarOpen) return <OpenSidebar />;
@@ -15,17 +14,7 @@ const Sidebar = () => {
       {/* top side */}
       <SidebarTop />
       {/* bot side */}
-      <div>
-        <div className="flex items-center justify-center gap-5 mb-5 bg-boardColor py-[.9rem] max-w-[90%] rounded-lg">
-          <img src={lightMode} alt="light mode" />
-          <label className="relative inline-flex items-center cursor-pointer">
-            <input type="checkbox" value="" className="sr-only peer" />
-            <div className="w-11 h-6 bg-mediumPurple peer-focus:outline-none peer-focus:ring-4 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-mediumPurple"></div>
-          </label>
-          <img src={darkMode} alt="dark mode" />
-        </div>
-        <HideSidebarBtn />
-      </div>
+      <SidebarBot />
     </aside>
   );
 };
---
+++ src/features/sidebar/SidebarBot.tsx
@@ -0,0 +1,21 @@
+import lightMode from "/assets/light-mode.svg";
+import darkMode from "/assets/dark-mode.svg";
+import HideSidebarBtn from "./HideSidebarBtn";
+
+const SidebarBot = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-center gap-5 mb-5 bg-boardColor py-[.9rem] max-w-[90%] rounded-lg">
+        <img src={lightMode} alt="light mode" />
+        <label className="relative inline-flex items-center cursor-pointer">
+          <input type="checkbox" value="" className="sr-only peer" />
+          <div className="w-11 h-6 bg-mediumPurple peer-focus:outline-none peer-focus:ring-4 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-mediumPurple"></div>
+        </label>
+        <img src={darkMode} alt="dark mode" />
+      </div>
+      <HideSidebarBtn />
+    </div>
+  );
+};
+
+export default SidebarBot;
