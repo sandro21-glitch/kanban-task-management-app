9ad566f20cdd16b222d4ab3807e11a82c2bb9249
Sandro <vipsandro11@gmail.com>
2024-01-17 20:33:24 +0400

    feat: create TaskDetails component

---
+++ src/features/board/TaskDetails.tsx
@@ -0,0 +1,47 @@
+import { useAppSelector } from "../../hooks/reduxHooks";
+
+type TaskDetailsTypes = {
+  todoTasks: {
+    taskName: string;
+    taskDesc: string;
+    subtasks: {
+      subtaskName: string;
+      isCompleted: boolean;
+    }[];
+  }[];
+};
+const TaskDetails = ({ todoTasks }: TaskDetailsTypes) => {
+  const darkMode = useAppSelector((store) => store.theme.darkMode);
+  return (
+    <div
+      className={`w-[280px] ${
+        darkMode ? "bg-darkMode" : "bg-white"
+      } mt-5 py-6 px-4 rounded-lg shadow-xl cursor-pointer group`}
+    >
+      {todoTasks.map((task, index) => {
+        const { taskName, subtasks } = task;
+        // Filter completed subtasks
+        const completedSubtasks = subtasks.filter(
+          (subtask) => subtask.isCompleted
+        );
+        return (
+          <div key={index}>
+            <p
+              className={`group-hover:text-mediumPurple
+             ${
+               darkMode ? "text-white" : "text-black"
+            } text-[.9rem] font-semibold transition-colors ease-in duration-150`}
+            >
+              {taskName}
+            </p>
+            <p>
+              {completedSubtasks.length} of {subtasks.length}
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default TaskDetails;
