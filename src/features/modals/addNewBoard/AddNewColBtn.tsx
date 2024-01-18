e4e91a137ec5455931f887a6ed78c2ca722de4cb
Sandro <vipsandro11@gmail.com>
2024-01-18 16:42:37 +0400

    feat: Implement 'AddNewColBtn' component for adding a new column

---
+++ src/features/modals/addNewBoard/AddNewColBtn.tsx
@@ -0,0 +1,12 @@
+const AddNewColBtn = () => {
+  return (
+    <button
+      className="rounded-full w-full bg-sidebarHover py-2 mb-6
+     text-mediumPurple font-bold hover:bg-transparent transition-all ease-in duration-150"
+    >
+      + Add New Column
+    </button>
+  );
+};
+
+export default AddNewColBtn;
