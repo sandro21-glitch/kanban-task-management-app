9747dd38207afba5b29bd13fc7a5e45e65bcf875
Sandro <vipsandro11@gmail.com>
2024-01-18 16:43:09 +0400

    feat: Implement 'CreateNewBoardBtn' component for creating a new board

---
+++ src/features/modals/addNewBoard/CreateNewBoardBtn.tsx
@@ -0,0 +1,12 @@
+const CreateNewBoardBtn = () => {
+  return (
+    <button
+      className="capitalize rounded-full bg-mediumPurple text-white font-bold
+     w-full py-2 hover:opacity-80 transition-opacity ease-in duration-150"
+    >
+      Create New Board
+    </button>
+  );
+};
+
+export default CreateNewBoardBtn;
