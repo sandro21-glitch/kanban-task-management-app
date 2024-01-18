de4ab26a1f90f3d7b3b90ec71a9e08d99f24f2c9
Sandro <vipsandro11@gmail.com>
2024-01-18 19:50:07 +0400

    feat: Integrate dynamic board name in BoardName component

--- src/features/modals/addNewBoard/BoardName.tsx
+++ src/features/modals/addNewBoard/BoardName.tsx
@@ -1,6 +1,9 @@
 import { useAppSelector } from "../../../hooks/reduxHooks";
-
-const BoardName = () => {
+type BoardNameTypes = {
+  setBoardName: (name: string) => void;
+  boardName: string;
+};
+const BoardName = ({ setBoardName, boardName }: BoardNameTypes) => {
   const darkMode = useAppSelector((store) => store.theme.darkMode);
   return (
     <div className="flex flex-col mb-5">
@@ -15,6 +18,8 @@ const BoardName = () => {
       <input
         type="text"
         placeholder="e.g Web Development"
+        value={boardName}
+        onChange={(e) => setBoardName(e.target.value)}
         className={`outline-none border border-borderColor p-2 text-[.9rem] rounded-md ${
           darkMode ? "bg-darkMode text-white" : "bg-white text-black"
         }`}
