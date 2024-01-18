54266f6bb9563d19ef7dd698b3be49d61a2e3cb2
Sandro <vipsandro11@gmail.com>
2024-01-18 16:43:38 +0400

    feat: Enhance 'CreateNewBoardBtn' component to dispatch 'setAddBoardModal' action from modalsSlice

--- src/ui/CreateNewBoardBtn.tsx
+++ src/ui/CreateNewBoardBtn.tsx
@@ -1,8 +1,15 @@
+import { setAddBoardModal } from "../features/modals/modalsSlice";
+import { useAppDispatch } from "../hooks/reduxHooks";
 import boardIcon from "/assets/board-icon.svg";
 
 const CreateNewBoardBtn = () => {
+  const dispatch = useAppDispatch();
+
   return (
-    <button className="text-mediumPurple font-bold flex items-center gap-5 mt-5">
+    <button
+      onClick={() => dispatch(setAddBoardModal())}
+      className="text-mediumPurple font-bold flex items-center gap-5 mt-5"
+    >
       <img src={boardIcon} alt="board icon" className="ml-5 gray-svg" />+ Create
       New Board
     </button>
