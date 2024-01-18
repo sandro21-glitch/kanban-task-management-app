ae197b1d5731493274247c1c049ee7da6c38909f
Sandro <vipsandro11@gmail.com>
2024-01-18 19:50:25 +0400

    feat: Make ColumnList dynamic with boardCols and setBoardCols integration

--- src/features/modals/addNewBoard/ColumnList.tsx
+++ src/features/modals/addNewBoard/ColumnList.tsx
@@ -1,38 +1,44 @@
 import { useAppSelector } from "../../../hooks/reduxHooks";
 import crossIcon from "/assets/icon-cross.svg";
-
-const ColumnList = () => {
+type ColumnListTypes = {
+  boardCols: string[];
+  setBoardCols: (cols: string[]) => void;
+};
+const ColumnList = ({ boardCols, setBoardCols }: ColumnListTypes) => {
   const darkMode = useAppSelector((store) => store.theme.darkMode);
+
+  const handleColumnChange = (index: number, value: string) => {
+    const newBoardCols = [...boardCols];
+    newBoardCols[index] = value;
+    setBoardCols(newBoardCols);
+  };
+
+
+
   return (
     <div className="mb-5">
       <label className="text-lightGray font-semibold text-[.8rem] mb-1">
         Board Columns
       </label>
       <ul className="flex flex-col gap-3">
-        <li className="flex items-center">
-          <input
-            type="text"
-            placeholder="e.g Todo"
-            className={`w-full outline-none border border-borderColor p-2 text-[.9rem] rounded-md ${
-              darkMode ? "bg-darkMode text-white" : "bg-white text-black"
-            }`}
-          />
-          <button type="button" className="ml-5">
-            <img src={crossIcon} alt="remove icon" />
-          </button>
-        </li>
-        <li className="flex items-center">
-          <input
-            type="text"
-            placeholder="e.g Todo"
-            className={`w-full outline-none border border-borderColor p-2 text-[.9rem] rounded-md ${
-              darkMode ? "bg-darkMode text-white" : "bg-white text-black"
-            }`}
-          />
-          <button type="button" className="ml-5">
-            <img src={crossIcon} alt="remove icon" />
-          </button>
-        </li>
+        {boardCols.map((col, index) => {
+          return (
+            <li key={index} className="flex items-center">
+              <input
+                type="text"
+                placeholder="e.g Todo"
+                value={col}
+                onChange={(e) => handleColumnChange(index, e.target.value)}
+                className={`w-full outline-none border border-borderColor p-2 text-[.9rem] rounded-md ${
+                  darkMode ? "bg-darkMode text-white" : "bg-white text-black"
+                }`}
+              />
+              <button type="button" className="ml-5">
+                <img src={crossIcon} alt="remove icon" />
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
