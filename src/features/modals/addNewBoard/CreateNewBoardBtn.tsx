6ea1b4275c074445acfb894c9afbd7647d714762
Sandro <vipsandro11@gmail.com>
2024-01-19 16:39:45 +0400

    chore: Add 'type="submit"' to CreateNewBoardBtn

--- src/features/modals/addNewBoard/CreateNewBoardBtn.tsx
+++ src/features/modals/addNewBoard/CreateNewBoardBtn.tsx
@@ -1,6 +1,7 @@
 const CreateNewBoardBtn = () => {
   return (
     <button
+      type="submit"
       className="capitalize rounded-full bg-mediumPurple text-white font-bold
      w-full py-2 hover:opacity-80 transition-opacity ease-in duration-150"
     >
