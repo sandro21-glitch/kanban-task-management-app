796c29fc515617ddf064b39c9060a0b63b27ebad
Sandro <vipsandro11@gmail.com>
2024-01-21 22:33:53 +0400

    refactor: Update BoardControls component

--- src/ui/BoardControls.tsx
+++ src/ui/BoardControls.tsx
@@ -1,9 +1,19 @@
+import EditBoardPopup from "../features/modals/editBoard/EditBoardPopup";
+import { openEditBoardPopModal } from "../features/modals/modalsSlice";
+import { useAppDispatch, useAppSelector } from "../hooks/reduxHooks";
 import iconVert from "/assets/icon-vertical-ellipsis.svg";
 
 const BoardControls = () => {
+  const dispatch = useAppDispatch();
+  const { openEditPopup } = useAppSelector((store) => store.modals);
+
   return (
-    <button>
+    <button
+      className="relative"
+      onClick={() => dispatch(openEditBoardPopModal(!openEditPopup))}
+    >
       <img src={iconVert} alt="icon vertical ellipsis" />
+      {openEditPopup && <EditBoardPopup />}
     </button>
   );
 };
