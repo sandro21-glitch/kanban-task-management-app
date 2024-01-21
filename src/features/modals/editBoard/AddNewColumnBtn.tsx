7baa6cec7aa3b84f04cd1ca44fdd6d234bd34bca
Sandro <vipsandro11@gmail.com>
2024-01-21 22:32:23 +0400

    style: Improve styling of AddNewColumnBtn component

---
+++ src/features/modals/editBoard/AddNewColumnBtn.tsx
@@ -0,0 +1,12 @@
+const AddNewColumnBtn = () => {
+  return (
+    <button
+      type="button"
+      className="bg-lightPurple mb-5 w-full rounded-full py-2 text-mediumPurple font-bold hover:bg-transparent transition-all ease-in duration-150"
+    >
+      + Add new column
+    </button>
+  );
+};
+
+export default AddNewColumnBtn;
