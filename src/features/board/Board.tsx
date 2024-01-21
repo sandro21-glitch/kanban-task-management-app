0ea66914f4154df7d2d2daf3fa26e33f2ef23066
Sandro <vipsandro11@gmail.com>
2024-01-21 22:45:47 +0400

    refactor: Update Board component to handle case when active board has no columns

--- src/features/board/Board.tsx
+++ src/features/board/Board.tsx
@@ -1,10 +1,15 @@
 import { useAppSelector } from "../../hooks/reduxHooks";
 import AddNewColumnBtn from "./AddNewColumnBtn";
 import BoardColumns from "./BoardColumns";
+import BoardNoCols from "./BoardNoCols";
+import { selectActiveBoard } from "./boardsSlice";
 
 const Board = () => {
   const darkMode = useAppSelector((store) => store.theme.darkMode);
-
+  const activeBoard = useAppSelector(selectActiveBoard);
+  if (activeBoard && activeBoard.boardTodos.length == 0) {
+    return <BoardNoCols />;
+  }
   return (
     <section
       className={`w-full h-[90vh] p-5 flex gap-5 ${
