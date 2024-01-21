462c40451801223f1bf652ae17dc517f4735fe46
Sandro <vipsandro11@gmail.com>
2024-01-21 22:31:58 +0400

    feat: Expand modalsSlice for additional modal functionality

--- src/features/modals/modalsSlice.ts
+++ src/features/modals/modalsSlice.ts
@@ -1,30 +1,36 @@
 import { createSlice } from "@reduxjs/toolkit";
-// import type { PayloadAction } from '@reduxjs/toolkit'
+import type { PayloadAction } from "@reduxjs/toolkit";
 
 export interface CounterState {
   addBoardModal: boolean;
   editBoardModal: boolean;
+  openEditPopup: boolean;
 }
 
 const initialState: CounterState = {
   addBoardModal: false,
-  editBoardModal: true,
+  editBoardModal: false,
+  openEditPopup: false,
 };
 
 export const modalsSlice = createSlice({
   name: "modals",
   initialState,
   reducers: {
-    setAddBoardModal: (state) => {
-      state.addBoardModal = !state.addBoardModal;
+    setAddBoardModal: (state, action: PayloadAction<boolean>) => {
+      state.addBoardModal = action.payload;
     },
-    setEditBoardModal: (state) => {
-      state.editBoardModal = !state.editBoardModal;
+    setEditBoardModal: (state, action: PayloadAction<boolean>) => {
+      state.editBoardModal = action.payload;
+    },
+    openEditBoardPopModal: (state, action: PayloadAction<boolean>) => {
+      state.openEditPopup = action.payload;
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { setAddBoardModal, setEditBoardModal } = modalsSlice.actions;
+export const { setAddBoardModal, setEditBoardModal, openEditBoardPopModal } =
+  modalsSlice.actions;
 
 export default modalsSlice.reducer;
