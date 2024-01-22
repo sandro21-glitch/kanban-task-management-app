2a94d889db062f9791057d39bda5222520bd78d8
Sandro <vipsandro11@gmail.com>
2024-01-22 20:05:34 +0400

    feat: Add `openClearPopup` state to Modals component in modalsSlice

--- src/features/modals/modalsSlice.ts
+++ src/features/modals/modalsSlice.ts
@@ -5,12 +5,14 @@ export interface CounterState {
   addBoardModal: boolean;
   editBoardModal: boolean;
   openEditPopup: boolean;
+  openClearPopup: boolean;
 }
 
 const initialState: CounterState = {
   addBoardModal: false,
   editBoardModal: false,
   openEditPopup: false,
+  openClearPopup: false,
 };
 
 export const modalsSlice = createSlice({
@@ -26,11 +28,18 @@ export const modalsSlice = createSlice({
     openEditBoardPopModal: (state, action: PayloadAction<boolean>) => {
       state.openEditPopup = action.payload;
     },
+    setOpenClearPopup: (state, action: PayloadAction<boolean>) => {
+      state.openClearPopup = action.payload;
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { setAddBoardModal, setEditBoardModal, openEditBoardPopModal } =
-  modalsSlice.actions;
+export const {
+  setAddBoardModal,
+  setEditBoardModal,
+  openEditBoardPopModal,
+  setOpenClearPopup,
+} = modalsSlice.actions;
 
 export default modalsSlice.reducer;
