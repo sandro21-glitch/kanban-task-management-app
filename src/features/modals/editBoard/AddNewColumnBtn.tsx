ebb7dc0faae2bfc01f5c2f93dcda4205efcc1613
Sandro <vipsandro11@gmail.com>
2024-01-22 16:23:54 +0400

    refactor: Modify AddNewColumnBtn to accept props for dynamic behavior

--- src/features/modals/editBoard/AddNewColumnBtn.tsx
+++ src/features/modals/editBoard/AddNewColumnBtn.tsx
@@ -1,6 +1,19 @@
-const AddNewColumnBtn = () => {
+type NewColBtnTypes = {
+  editBoardCols: string[];
+  setEditBoardCols: (col: string[]) => void;
+};
+
+const AddNewColumnBtn = ({
+  editBoardCols,
+  setEditBoardCols,
+}: NewColBtnTypes) => {
+  const handleAddNewCol = () => {
+    setEditBoardCols([...editBoardCols, "Todo"]);
+  };
+
   return (
     <button
+      onClick={handleAddNewCol}
       type="button"
       className="bg-lightPurple mb-5 w-full rounded-full py-2 text-mediumPurple font-bold hover:bg-transparent transition-all ease-in duration-150"
     >
