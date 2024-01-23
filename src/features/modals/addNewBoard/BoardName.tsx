dc60b1a462233d7284ee2d76a95f856965940703
Sandro <vipsandro11@gmail.com>
2024-01-23 19:55:20 +0400

    refactor: Use ModalLabel component in BoardName

--- src/features/modals/addNewBoard/BoardName.tsx
+++ src/features/modals/addNewBoard/BoardName.tsx
@@ -1,21 +1,13 @@
-import { useAppSelector } from "../../../hooks/reduxHooks";
 import ModalInput from "../../../ui/ModalInput";
+import ModalLabel from "../../../ui/ModalLabel";
 type BoardNameTypes = {
   setBoardName: (name: string) => void;
   boardName: string;
 };
 const BoardName = ({ setBoardName, boardName }: BoardNameTypes) => {
-  const darkMode = useAppSelector((store) => store.theme.darkMode);
   return (
     <div className="flex flex-col mb-5">
-      <label
-        htmlFor="boardName"
-        className={`${
-          darkMode ? "text-white" : "text-lightGray"
-        } font-semibold text-[.8rem] mb-1`}
-      >
-        Board Name
-      </label>
+      <ModalLabel forId="boardName" name="Board Name" />
       <ModalInput
         id="boardName"
         placeholder="e.g Web Development"
