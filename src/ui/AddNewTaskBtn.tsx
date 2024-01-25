8b14389842939bb77e8de97e778a0e5069651aa8
Sandro <vipsandro11@gmail.com>
2024-01-25 21:09:45 +0400

    refactor: Connect AddNewTaskBtn to modalsSlice for better state management

--- src/ui/AddNewTaskBtn.tsx
+++ src/ui/AddNewTaskBtn.tsx
@@ -1,6 +1,13 @@
+import { setNewTaskModal } from "../features/modals/modalsSlice";
+import { useAppDispatch } from "../hooks/reduxHooks";
+
 const AddNewTaskBtn = () => {
+  const dispatch = useAppDispatch();
   return (
-    <button className="bg-mediumPurple w-[164px] h-[3em] rounded-full text-white font-bold normal-case">
+    <button
+      onClick={() => dispatch(setNewTaskModal(true))}
+      className="bg-mediumPurple w-[164px] h-[3em] rounded-full text-white font-bold normal-case"
+    >
       + Add new task
     </button>
   );
