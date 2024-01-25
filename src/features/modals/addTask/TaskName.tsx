210ea7d9e6cfbe5a3f8d9f6b66e6b5e30cf83cc2
Sandro <vipsandro11@gmail.com>
2024-01-25 21:09:28 +0400

    refactor: Make TaskName a controlled component

--- src/features/modals/addTask/TaskName.tsx
+++ src/features/modals/addTask/TaskName.tsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
 import ModalInput from "../../../ui/ModalInput";
 import ModalLabel from "../../../ui/ModalLabel";
-
-const TaskName = () => {
-  const [taskName, setTaskName] = useState("");
+type TaskNameTypes = {
+  taskName: string;
+  setTaskName: (name: string) => void;
+};
+const TaskName = ({ taskName, setTaskName }: TaskNameTypes) => {
   return (
     <div className="flex flex-col gap-1">
       <ModalLabel forId="taskName" name="Task Name" />
