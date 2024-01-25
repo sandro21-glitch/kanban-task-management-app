bee02ff00eb92e6638afd9d7e7a4e5c243237a7f
Sandro <vipsandro11@gmail.com>
2024-01-25 21:08:26 +0400

    feat: Added CreateTaskBtn component

---
+++ src/features/modals/addTask/CreateTaskBtn.tsx
@@ -0,0 +1,14 @@
+const CreateTaskBtn = () => {
+  return (
+    <button
+      className="
+      bg-mediumPurple w-full py-2
+        rounded-full font-bold text-white hover:bg-[#a8a4ff]
+        transition-colors ease-in duration-150"
+    >
+      Create Task
+    </button>
+  );
+};
+
+export default CreateTaskBtn;
