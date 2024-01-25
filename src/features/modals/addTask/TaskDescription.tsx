4cd488426780b0a3ba19c8628a0f325a223447ee
Sandro <vipsandro11@gmail.com>
2024-01-25 21:09:14 +0400

    refactor: Make TaskDescription a controlled component

--- src/features/modals/addTask/TaskDescription.tsx
+++ src/features/modals/addTask/TaskDescription.tsx
@@ -1,13 +1,21 @@
 import { useAppSelector } from "../../../hooks/reduxHooks";
 import ModalLabel from "../../../ui/ModalLabel";
-
-const TaskDescription = () => {
+type TaskDescTypes = {
+  taskDescription: string;
+  setTaskDescription: (desc: string) => void;
+};
+const TaskDescription = ({
+  taskDescription,
+  setTaskDescription,
+}: TaskDescTypes) => {
   const darkMode = useAppSelector((store) => store.theme.darkMode);
   return (
     <div className="flex flex-col gap-1">
       <ModalLabel forId="desc" name="Description (optional)" />
       <textarea
         id="desc"
+        value={taskDescription}
+        onChange={(e) => setTaskDescription(e.target.value)}
         placeholder="e.g. Start learning Things"
         className={`outline-none border border-borderColor p-2 text-[.9rem] rounded-md ${
           darkMode ? "bg-darkMode text-white" : "bg-white text-black"
