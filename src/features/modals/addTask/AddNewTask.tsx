cfadaccc94531aec3f7a9c57d0054acb3ff9c1cc
Sandro <vipsandro11@gmail.com>
2024-01-25 21:08:10 +0400

    feat: :sparkles: Convert AddNewTask component to a controlled component
    - Removed the dependency on useAppSelector from reduxHooks - Imported useState from react for local state management - Created state variables for taskName, taskDescription, subtasks, and activeStatus - Passed state variables as props to child components TaskName, TaskDescription, Subtasks, and SelectCol - Added a button to trigger the handleAddSubtask function for testing

--- src/features/modals/addTask/AddNewTask.tsx
+++ src/features/modals/addTask/AddNewTask.tsx
@@ -1,13 +1,47 @@
-import { useAppSelector } from "../../../hooks/reduxHooks";
+import { useState } from "react";
+import { useAppDispatch, useAppSelector } from "../../../hooks/reduxHooks";
 import TaskName from "./TaskName";
 import TaskDescription from "./TaskDescription";
 import Subtasks from "./Subtasks";
 import SelectCol from "./SelectCol";
-
+import CreateTaskBtn from "./CreateTaskBtn";
+import { addNewTask } from "../../board/boardsSlice";
+export interface ActiveStatus {
+  statusName: string;
+  index: number | null;
+}
 const AddNewTask = () => {
+  const dispatch = useAppDispatch();
   const darkMode = useAppSelector((store) => store.theme.darkMode);
+  const [taskName, setTaskName] = useState("");
+  const [taskDescription, setTaskDescription] = useState("");
+  const [subtasks, setSubtasks] = useState<string[]>([]);
+  const [activeStatus, setActiveStatus] = useState<ActiveStatus>({
+    statusName: "",
+    index: null,
+  });
+
+  const todoTask = {
+    taskName: taskName,
+    taskDesc: taskDescription,
+    subtasks: subtasks.map((subtask) => ({
+      subtaskName: subtask,
+      isCompleted: false,
+    })),
+  };
+
+  const handleAddSubtask = () => {
+    if (
+      taskName !== "" &&
+      subtasks.length >= 1 &&
+      activeStatus.index !== null
+    ) {
+      dispatch(addNewTask({ todoTask, activeStatus }));
+    }
+  };
+
   return (
-    <div
+    <article
       className={`min-w-[90vw] sm:min-w-[670px] ${
         darkMode ? "bg-darkMode" : "bg-white"
       } p-7 rounded-lg`}
@@ -20,12 +54,20 @@ const AddNewTask = () => {
         Add New Task
       </h3>
       <div className="flex flex-col gap-5">
-        <TaskName />
-        <TaskDescription />
-        <Subtasks />
-        <SelectCol />
+        <TaskName taskName={taskName} setTaskName={setTaskName} />
+        <TaskDescription
+          taskDescription={taskDescription}
+          setTaskDescription={setTaskDescription}
+        />
+        <Subtasks subtasks={subtasks} setSubtasks={setSubtasks} />
+        <SelectCol
+          activeStatus={activeStatus}
+          setActiveStatus={setActiveStatus}
+        />
+        <CreateTaskBtn />
+        <button onClick={() => handleAddSubtask()}>Click test</button>
       </div>
-    </div>
+    </article>
   );
 };
 
