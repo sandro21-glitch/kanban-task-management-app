7f16408aa276696873d40198c590c272437f6a90
Sandro <vipsandro11@gmail.com>
2024-01-29 21:07:31 +0400

    feat: :sparkles: Add modal close functionality in TaskOptions
    - Introduce useRef, useEffect, and useCallback to handle modal clicks - Close the TaskOptions modal when clicking outside its boundaries - Prevent modal closure on clicks inside the modal content

--- src/features/modals/taskSubtasks/TaskOptions.tsx
+++ src/features/modals/taskSubtasks/TaskOptions.tsx
@@ -1,19 +1,66 @@
-import { useAppSelector } from "../../../hooks/reduxHooks";
+import { useRef, useEffect, useCallback } from "react";
+import { useAppDispatch, useAppSelector } from "../../../hooks/reduxHooks";
 import OptionsHeader from "./OptionsHeader";
 import SubtasksList from "./SubtasksList";
+import { setOpenTaskOptions } from "../modalsSlice";
 
 type TaskOptionTypes = {
   taskName: string;
   taskDesc?: string;
   subtasks: { subtaskName: string; isCompleted: boolean }[];
 };
+
 const TaskOptions = ({ taskName, taskDesc, subtasks }: TaskOptionTypes) => {
+  const dispatch = useAppDispatch();
+  const { openTaskOptions } = useAppSelector((store) => store.modals);
   const darkMode = useAppSelector((store) => store.theme.darkMode);
   const completedSubs = subtasks.filter((task) => task.isCompleted).length;
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  const closeModal = () => {
+    dispatch(setOpenTaskOptions(false));
+  };
+
+  const handleOutsideClick = useCallback(
+    (event: MouseEvent) => {
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(event.target as Node)
+      ) {
+        closeModal();
+      }
+    },
+    [closeModal]
+  );
+
+  const handleInsideClick = useCallback(
+    (event: React.MouseEvent<HTMLDivElement>) => {
+      // Prevent clicks inside the modal from closing it
+      event.stopPropagation();
+    },
+    []
+  );
+
+  useEffect(() => {
+    if (openTaskOptions) {
+      document.addEventListener("mousedown", handleOutsideClick);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [openTaskOptions, handleOutsideClick]);
+
+  if (!openTaskOptions) return null;
+
   return (
     <div className="w-screen h-screen fixed inset-0 z-[99999] flex justify-center">
-      <div className=" bg-black opacity-40 absolute inset-0 z-[-1]"></div>
-      <section className="flex justify-center items-center h-fit my-auto">
+      <div className="bg-black opacity-40 absolute inset-0 z-[-1]"></div>
+      <section
+        ref={modalRef}
+        className="flex justify-center items-center h-fit my-auto"
+        onMouseDown={handleInsideClick}
+      >
         <div
           className={`min-w-[90vw] sm:min-w-[500px] ${
             darkMode ? "bg-darkMode" : "bg-white"
