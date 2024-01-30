1ab906ca65b7a5d612c7bce11aaf33b792a7f3ce
Sandro <vipsandro11@gmail.com>
2024-01-30 20:55:33 +0400

    chore: Add todoId to SubtasksList props

--- src/features/modals/taskSubtasks/SubtasksList.tsx
+++ src/features/modals/taskSubtasks/SubtasksList.tsx
@@ -5,15 +5,18 @@ type SubtaskLists = {
     subtaskName: string;
     isCompleted: boolean;
   }[];
+  todoId: string;
 };
 
-const SubtasksList = ({ subtasks }: SubtaskLists) => {
+const SubtasksList = ({ subtasks, todoId }: SubtaskLists) => {
   return (
     <ul className="flex flex-col gap-3">
       {subtasks.map((subtask, index) => {
         return (
           <SingleSubtask
             key={index}
+            todoId={todoId}
+            index={index}
             completed={subtask.isCompleted}
             subtaskName={subtask.subtaskName}
           />
