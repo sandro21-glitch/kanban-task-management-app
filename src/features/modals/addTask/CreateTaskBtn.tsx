edb98cba7714eb94163413d28959ca060bc3de52
Sandro <vipsandro11@gmail.com>
2024-01-31 19:13:36 +0400

    feat: Update CreateTaskBtn component to accept a callback function

--- src/features/modals/addTask/CreateTaskBtn.tsx
+++ src/features/modals/addTask/CreateTaskBtn.tsx
@@ -1,6 +1,10 @@
-const CreateTaskBtn = () => {
+type CreateTaskBtnTypes = {
+  handleAddSubtask: () => void;
+};
+const CreateTaskBtn = ({ handleAddSubtask }: CreateTaskBtnTypes) => {
   return (
     <button
+      onClick={() => handleAddSubtask()}
       className="
       bg-mediumPurple w-full py-2
         rounded-full font-bold text-white hover:bg-[#a8a4ff]
