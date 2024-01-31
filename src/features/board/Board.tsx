8a7a9beff14cb0abb08f2be5cd4f1a66dc1b5709
Sandro <vipsandro11@gmail.com>
2024-01-31 19:14:43 +0400

    feat: Display NoBoards  component  when no boards are available

--- src/features/board/Board.tsx
+++ src/features/board/Board.tsx
@@ -1,11 +1,15 @@
 import { useAppSelector } from "../../hooks/reduxHooks";
-import AddNewColumnBtn from "./AddNewColumnBtn";
 import BoardColumns from "./BoardColumns";
+import AddNewColumnBtn from "./AddNewColumnBtn";
+
 import BoardNoCols from "./BoardNoCols";
 import { selectActiveBoard } from "./boardsSlice";
+import NoBoards from "./NoBoards";
 
 const Board = () => {
   const darkMode = useAppSelector((store) => store.theme.darkMode);
+  const { boards } = useAppSelector((store) => store.board);
+
   const activeBoard = useAppSelector(selectActiveBoard);
   if (activeBoard && activeBoard.boardTodos.length == 0) {
     return <BoardNoCols />;
@@ -16,8 +20,14 @@ const Board = () => {
         darkMode ? "bg-boardColorDark" : "bg-boardColor"
       }`}
     >
-      <BoardColumns />
-      <AddNewColumnBtn />
+      {boards.length < 1 ? (
+        <NoBoards />
+      ) : (
+        <>
+          <BoardColumns />
+          <AddNewColumnBtn />
+        </>
+      )}
     </section>
   );
 };
