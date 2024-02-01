e700c9d1996dde0eb5dad7471c1e4adafefb8a59
Sandro <vipsandro11@gmail.com>
2024-02-01 20:22:02 +0400

    refactor: Update OptionsHeader component to use dark mode from Redux store

--- src/features/modals/taskSubtasks/OptionsHeader.tsx
+++ src/features/modals/taskSubtasks/OptionsHeader.tsx
@@ -1,12 +1,16 @@
+import { useAppSelector } from "../../../hooks/reduxHooks";
 import vertIcon from "/assets/icon-vertical-ellipsis.svg";
 
 type HeaderTypes = {
   taskName: string;
 };
 const OptionsHeader = ({ taskName }: HeaderTypes) => {
+  const darkMode = useAppSelector((store) => store.theme.darkMode);
   return (
     <div className="flex justify-between items-center mb-5 cursor-text">
-      <h5 className="font-bold">{taskName}</h5>
+      <h5 className={`font-bold ${darkMode ? "text-white" : "text-black"}`}>
+        {taskName}
+      </h5>
       <button>
         <img src={vertIcon} alt="vertical icon" />
       </button>
