e8ed665d6c67822ae2fa76c2bd95fbe685d6114a
Sandro <vipsandro11@gmail.com>
2024-02-05 18:55:51 +0400

    feat: Integrate TaskSettings in TaskOptions

--- src/features/modals/taskSubtasks/TaskOptions.tsx
+++ src/features/modals/taskSubtasks/TaskOptions.tsx
@@ -2,6 +2,8 @@ import { useAppSelector } from "../../../hooks/reduxHooks";
 import OptionsHeader from "./OptionsHeader";
 import SubtasksList from "./SubtasksList";
 import SubtasksCurrStatus from "./SubtasksCurrStatus";
+import TaskSettings from "./taskSettingsControl/TaskSettings";
+import { useState } from "react";
 
 type TaskOptionTypes = {
   taskName: string;
@@ -20,22 +22,34 @@ const TaskOptions = ({
 }: TaskOptionTypes) => {
   const darkMode = useAppSelector((store) => store.theme.darkMode);
   const completedSubs = subtasks.filter((task) => task.isCompleted).length;
-  
+  const [isSettingsOpen, setIsSettingsOpen] = useState<boolean>(false);
   return (
     <div
       className={`min-w-[90vw] sm:min-w-[500px] ${
         darkMode ? "bg-darkMode" : "bg-white"
-      } p-7 rounded-lg cursor-default`}
+      } p-7 rounded-lg cursor-default relative`}
     >
-      <OptionsHeader taskName={taskName} />
-      <p className={`${darkMode ? 'text-white' : 'text-black' } text-[.8rem] mb-5 cursor-text`}>
+      <OptionsHeader
+        taskName={taskName}
+        setIsSettingsOpen={setIsSettingsOpen}
+      />
+      <p
+        className={`${
+          darkMode ? "text-white" : "text-black"
+        } text-[.8rem] mb-5 cursor-text`}
+      >
         {taskDesc ? taskDesc : ""}
       </p>
-      <p className={`${darkMode ? 'text-white' : 'text-black'} text-[.75rem] font-bold mb-2`}>
+      <p
+        className={`${
+          darkMode ? "text-white" : "text-black"
+        } text-[.75rem] font-bold mb-2`}
+      >
         Subtasks ({completedSubs} of {subtasks.length})
       </p>
       <SubtasksList subtasks={subtasks} todoId={todoId} />
       <SubtasksCurrStatus todoId={todoId} taskId={taskId} />
+      {isSettingsOpen && <TaskSettings taskId={taskId} />}
     </div>
   );
 };
