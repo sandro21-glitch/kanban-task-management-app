982f7f840d780f46a6366289cf43496e07677340
Sandro <vipsandro11@gmail.com>
2024-02-05 18:55:35 +0400

    feat: Add setIsSettingsOpen prop to OptionsHeader

--- src/features/modals/taskSubtasks/OptionsHeader.tsx
+++ src/features/modals/taskSubtasks/OptionsHeader.tsx
@@ -3,15 +3,16 @@ import vertIcon from "/assets/icon-vertical-ellipsis.svg";
 
 type HeaderTypes = {
   taskName: string;
+  setIsSettingsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
-const OptionsHeader = ({ taskName }: HeaderTypes) => {
+const OptionsHeader = ({ taskName, setIsSettingsOpen }: HeaderTypes) => {
   const darkMode = useAppSelector((store) => store.theme.darkMode);
   return (
     <div className="flex justify-between items-center mb-5 cursor-text">
       <h5 className={`font-bold ${darkMode ? "text-white" : "text-black"}`}>
         {taskName}
       </h5>
-      <button>
+      <button type="button" onClick={() => setIsSettingsOpen((prev) => !prev)}>
         <img src={vertIcon} alt="vertical icon" />
       </button>
     </div>
