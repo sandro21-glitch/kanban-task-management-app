ae7c26382cf61dab7306d0217738c2c00e7f50c9
Sandro <vipsandro11@gmail.com>
2024-02-07 20:23:54 +0400

    feat: Enhance BoardName to handle form submission status and display input errors

--- src/features/modals/addNewBoard/BoardName.tsx
+++ src/features/modals/addNewBoard/BoardName.tsx
@@ -3,8 +3,9 @@ import ModalLabel from "../../../ui/ModalLabel";
 type BoardNameTypes = {
   setBoardName: (name: string) => void;
   boardName: string;
+  submitted: boolean;
 };
-const BoardName = ({ setBoardName, boardName }: BoardNameTypes) => {
+const BoardName = ({ setBoardName, boardName, submitted }: BoardNameTypes) => {
   return (
     <div className="flex flex-col mb-5">
       <ModalLabel forId="boardName" name="Board Name" />
@@ -13,6 +14,7 @@ const BoardName = ({ setBoardName, boardName }: BoardNameTypes) => {
         placeholder="e.g Web Development"
         value={boardName}
         onChange={setBoardName}
+        isSubmited={submitted}
       />
     </div>
   );
