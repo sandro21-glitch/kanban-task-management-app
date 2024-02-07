19de12ab1491850d2358b2996f6d405e20266aa6
Sandro <vipsandro11@gmail.com>
2024-02-07 20:20:47 +0400

    refactor: Enhance ModalInput to display error on form submission

--- src/ui/ModalInput.tsx
+++ src/ui/ModalInput.tsx
@@ -5,22 +5,38 @@ type InputTypes = {
   placeholder: string;
   value: string;
   onChange: (value: string) => void;
+  isSubmited: boolean;
 };
 
-const ModalInput = ({ id, placeholder, value, onChange }: InputTypes) => {
+const ModalInput = ({
+  id,
+  placeholder,
+  value,
+  onChange,
+  isSubmited,
+}: InputTypes) => {
   const darkMode = useAppSelector((store) => store.theme.darkMode);
 
   return (
-    <input
-      id={id}
-      type="text"
-      placeholder={placeholder}
-      value={value}
-      onChange={(e) => onChange(e.target.value)}
-      className={`outline-none border border-borderColor p-2 text-[.9rem] rounded-md ${
-        darkMode ? "bg-darkMode text-white" : "bg-white text-black"
-      }`}
-    />
+    <div className="w-full relative">
+      <input
+        id={id}
+        type="text"
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        className={`${
+          !value && isSubmited ? "border-red-500" : ""
+        } outline-none border border-borderColor p-2 text-[.9rem] rounded-md ${
+          darkMode ? "bg-darkMode text-white" : "bg-white text-black w-full"
+        }`}
+      />
+      {!value && isSubmited && (
+        <p className="text-red-500 absolute right-3 top-3 text-[.75rem]">
+          Can't be empty
+        </p>
+      )}
+    </div>
   );
 };
 
