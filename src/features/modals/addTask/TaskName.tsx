3893609bc9dd06f329339d4e9d3a5585c242b7cf
Sandro <vipsandro11@gmail.com>
2024-02-07 20:22:17 +0400

    feat: Enhance TaskName to handle form submission status

--- src/features/modals/addTask/TaskName.tsx
+++ src/features/modals/addTask/TaskName.tsx
@@ -3,8 +3,9 @@ import ModalLabel from "../../../ui/ModalLabel";
 type TaskNameTypes = {
   taskName: string;
   setTaskName: (name: string) => void;
+  submitted: boolean;
 };
-const TaskName = ({ taskName, setTaskName }: TaskNameTypes) => {
+const TaskName = ({ taskName, setTaskName, submitted }: TaskNameTypes) => {
   return (
     <div className="flex flex-col gap-1">
       <ModalLabel forId="taskName" name="Task Name" />
@@ -12,6 +13,7 @@ const TaskName = ({ taskName, setTaskName }: TaskNameTypes) => {
         id="taskName"
         value={taskName}
         onChange={setTaskName}
+        isSubmited={submitted}
         placeholder="e.g Take coffe break"
       />
     </div>
