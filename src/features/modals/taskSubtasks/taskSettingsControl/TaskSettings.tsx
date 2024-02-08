fe2087faccb497533a78ca1e5beec069544a436d
Sandro <vipsandro11@gmail.com>
2024-02-08 18:26:49 +0400

    refactor: Extend TaskSettings to include todoId in EditTaskBtn

--- src/features/modals/taskSubtasks/taskSettingsControl/TaskSettings.tsx
+++ src/features/modals/taskSubtasks/taskSettingsControl/TaskSettings.tsx
@@ -3,15 +3,16 @@ import EditTaskBtn from "./EditTaskBtn";
 
 type TaskSettingTypes = {
   taskId: string;
+  todoId: string;
 };
 
-const TaskSettings = ({ taskId }: TaskSettingTypes) => {
+const TaskSettings = ({ taskId, todoId }: TaskSettingTypes) => {
   return (
     <div
       className="absolute top-16 right-[-85px] w-[192px] shadow-modal bg-white p-4 z-[9999] rounded-[8px]
     flex flex-col gap-2 items-start"
     >
-      <EditTaskBtn />
+      <EditTaskBtn todoId={todoId} taskId={taskId} />
       <DeleteTaskBtn taskId={taskId} />
     </div>
   );
