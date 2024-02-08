315362f13f8fe839d5ca6ae404713a8433291b02
Sandro <vipsandro11@gmail.com>
2024-02-08 18:26:05 +0400

    feat: Implement EditTask component with form for updating task details

--- src/features/modals/editTask/EditTask.tsx
+++ src/features/modals/editTask/EditTask.tsx
@@ -1,36 +1,89 @@
 import { useState } from "react";
-import { useAppSelector } from "../../../hooks/reduxHooks";
+import { useAppDispatch, useAppSelector } from "../../../hooks/reduxHooks";
 import EditTitle from "./EditTitle";
 import EditDescription from "./EditDescription";
 import EditSubtasks from "./EditSubtasks";
+import EditStatus from "./EditStatus";
+import { editSubtaskDetails } from "../../board/boardsSlice";
+import { SingleTaskTypes, SubtaskType } from "../../../types/BoardTypes";
+import { openEditTaskModal } from "../modalsSlice";
 
-const EditTask = () => {
+type EditTaskTypes = {
+  taskId: string;
+  todoId: string;
+};
+
+const EditTask = ({ taskId, todoId }: EditTaskTypes) => {
   const darkMode = useAppSelector((store) => store.theme.darkMode);
-  const [editedTitle, setEditedTitle] = useState<string>("");
-  const [editedDesc, setEditedDesc] = useState<string>("");
-//   const [editedSubtasks,setEditedSubtasks] = useState<string[]>([])
+  const { boards } = useAppSelector((store) => store.board);
+  const dispatch = useAppDispatch();
+
+  const activeTodo = boards
+    .flatMap((board) => board.boardTodos)
+    .find((todo) => todo.todoId === todoId);
+
+  const activeTask = activeTodo?.todoTasks.find(
+    (task) => task.taskId === taskId
+  );
+  const [editedTitle, setEditedTitle] = useState<string>(
+    activeTask ? activeTask.taskName : ""
+  );
+  const [editedDesc, setEditedDesc] = useState<string>(
+    activeTask ? activeTask.taskDesc : ""
+  );
+  const [editedSubtasks, setEditedSubtasks] = useState<SubtaskType[]>(
+    (activeTask?.subtasks || []).map((subtask) => ({
+      subtaskName: subtask.subtaskName,
+      subtaskId: subtask.subtaskId,
+      isCompleted: subtask.isCompleted,
+    }))
+  );
+
+  const updatedTask: SingleTaskTypes = {
+    taskName: editedTitle,
+    taskDesc: editedDesc,
+    taskId,
+    todoId,
+    subtasks: editedSubtasks,
+  };
+  const handleEditTask = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch(editSubtaskDetails(updatedTask));
+    dispatch(openEditTaskModal({ isOpen: false, taskId: "", todoId: "" }));
+  };
+
   return (
-    <article
-      className={`min-w-[90vw] sm:min-w-[700px] ${
-        darkMode ? "bg-darkMode" : "bg-white"
-      } p-7 rounded-lg`}
-    >
-      <h3
-        className={`${
-          darkMode ? "text-white" : "text-black"
-        } font-semibold mb-5 text-[1.3rem]`}
+    <form onSubmit={handleEditTask}>
+      <div
+        className={`min-w-[90vw] sm:min-w-[700px] ${
+          darkMode ? "bg-darkMode" : "bg-white"
+        } p-7 rounded-lg `}
       >
-        Edit Task
-      </h3>
-      <div>
-        <EditTitle editedTitle={editedTitle} setEditedTitle={setEditedTitle} />
-        <EditDescription
-          editedDesc={editedDesc}
-          setEditedDesc={setEditedDesc}
-        />
-        <EditSubtasks />
+        <h3
+          className={`${
+            darkMode ? "text-white" : "text-black"
+          } font-semibold mb-5 text-[1.3rem]`}
+        >
+          Edit Task
+        </h3>
+        <article>
+          <EditTitle
+            editedTitle={editedTitle}
+            setEditedTitle={setEditedTitle}
+          />
+          <EditDescription
+            editedDesc={editedDesc}
+            setEditedDesc={setEditedDesc}
+          />
+          <EditSubtasks
+            editedSubtasks={editedSubtasks}
+            setEditedSubtasks={setEditedSubtasks}
+          />
+          <EditStatus />
+        </article>
+        <button type="submit">Save Changes</button>
       </div>
-    </article>
+    </form>
   );
 };
 
