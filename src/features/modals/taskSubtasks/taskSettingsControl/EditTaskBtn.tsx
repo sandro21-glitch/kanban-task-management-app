d77481e85c854cd17b1a06b02285f9d2395a5a97
Sandro <vipsandro11@gmail.com>
2024-02-08 18:26:34 +0400

    feat: Introduce dynamic behavior to EditTaskBtn

--- src/features/modals/taskSubtasks/taskSettingsControl/EditTaskBtn.tsx
+++ src/features/modals/taskSubtasks/taskSettingsControl/EditTaskBtn.tsx
@@ -1,10 +1,27 @@
+import { useAppDispatch } from "../../../../hooks/reduxHooks";
+import { openEditTaskModal, setOpenTaskOptions } from "../../modalsSlice";
 
-const EditTaskBtn = () => {
+type EditTaskBtnTypes = {
+  taskId: string;
+  todoId: string;
+};
+
+const EditTaskBtn = ({ taskId, todoId }: EditTaskBtnTypes) => {
+  const dispatch = useAppDispatch();
+
+  const handleEditTask = () => {
+    dispatch(setOpenTaskOptions({ isOpen: false, task: null }));
+    dispatch(openEditTaskModal({ isOpen: true, taskId, todoId }));
+  };
   return (
-    <button className="text-[.8rem] leading-[23px] text-gray-500 font-semibold w-full text-start hover:text-gray-700 transition-colors ease-linear duration-150">
-        Edit Task
-      </button>
-  )
-}
+    <button
+      type="button"
+      onClick={handleEditTask}
+      className="text-[.8rem] leading-[23px] text-gray-500 font-semibold w-full text-start hover:text-gray-700 transition-colors ease-linear duration-150"
+    >
+      Edit Task
+    </button>
+  );
+};
 
-export default EditTaskBtn
+export default EditTaskBtn;
