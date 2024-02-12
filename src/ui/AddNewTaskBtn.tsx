1aeca7d64ce155eca1decaae91950c61ac76aeee
Sandro <vipsandro11@gmail.com>
2024-02-12 18:57:41 +0400

    style: Improve responsiveness and text size in AddNewTaskBtn component

--- src/ui/AddNewTaskBtn.tsx
+++ src/ui/AddNewTaskBtn.tsx
@@ -6,7 +6,8 @@ const AddNewTaskBtn = () => {
   return (
     <button
       onClick={() => dispatch(setNewTaskModal(true))}
-      className="bg-mediumPurple sm:w-[164px] w-[3em] h-[2em] sm:h-[3em] rounded-full text-white font-bold normal-case"
+      className="bg-mediumPurple sm:text-xs md:text-[1rem] sm:w-[110px] md:w-[164px] w-[3em] h-[2em] sm:h-[3em]
+       rounded-full text-white font-bold normal-case"
     >
       <span className="hidden sm:inline">+ Add new task</span>
       <span className="sm:hidden">+</span>
