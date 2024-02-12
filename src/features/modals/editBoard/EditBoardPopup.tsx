9c2594b7e4498650e6f3809cf0ffcd1b80d47197
Sandro <vipsandro11@gmail.com>
2024-02-12 19:15:20 +0400

    style: Enhance positioning of EditBoardPopup for better UI

--- src/features/modals/editBoard/EditBoardPopup.tsx
+++ src/features/modals/editBoard/EditBoardPopup.tsx
@@ -9,7 +9,7 @@ const EditBoardPopup = () => {
   const dispatch = useAppDispatch();
   return (
     <ul
-      className="bg-white absolute right-0 top-[4rem] shadow-lg rounded-lg z-[9999] w-[192px] p-3
+      className="bg-white absolute right-0 top-[3rem] sm:top-[4rem] shadow-lg rounded-lg z-[9999] w-[192px] p-3
       flex flex-col items-start gap-5"
     >
       <li
@@ -30,7 +30,6 @@ const EditBoardPopup = () => {
       >
         Delete Board
       </li>
-      <li className="text-[.8rem] font-medium text-red-500">Reset Boards</li>
     </ul>
   );
 };
