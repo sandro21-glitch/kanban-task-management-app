a148c2fa78780a3c3f9cec025aec173928604a3e
Sandro <vipsandro11@gmail.com>
2024-02-13 18:23:47 +0400

    refactor: updated import statements in Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import Board from "../features/board/Board";
 import Sidebar from "../features/sidebar/Sidebar";
+
 const Home = () => {
   return (
     <section className="flex w-full absolute bottom-0 top-[1rem] left-0 sm:top-14">
