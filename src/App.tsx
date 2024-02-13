a6aaa6f0684f1b3b65ad6a49ef6ae4f10b967231
Sandro <vipsandro11@gmail.com>
2024-02-13 15:18:40 +0400

    refactor: Update App component with useEffect for fetching new board

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { useEffect } from "react";
 import Modals from "./features/modals/Modals";
 import Home from "./pages/Home";
 import Navbar from "./ui/Navbar";
-
+import { useAppDispatch } from "./hooks/reduxHooks";
+import { fetchNewBoard } from "./features/board/boardsSlice";
+import data from "./data.json";
 function App() {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(fetchNewBoard(data));
+  }, [dispatch]);
   return (
     <main>
       <Navbar />
