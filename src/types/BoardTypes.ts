a3687ee396d6ae0b747210051de938d63cee9b1a
Sandro <vipsandro11@gmail.com>
2024-02-13 15:21:04 +0400

    feat: Introduce SingleBoard interface in BoardTypes

--- src/types/BoardTypes.ts
+++ src/types/BoardTypes.ts
@@ -20,6 +20,26 @@ export interface BoardState {
   }[];
 }
 
+export interface SingleBoard {
+  id: number;
+  name: string;
+  isActive: boolean;
+  boardTodos: {
+    todoId: string;
+    todoName: string;
+    todoTasks: {
+      taskName: string;
+      taskDesc: string;
+      taskId: string;
+      subtasks: {
+        subtaskId: string;
+        subtaskName: string;
+        isCompleted: boolean;
+      }[];
+    }[];
+  }[];
+}
+
 export interface BoardTodos {
   todoId: string;
   todoName: string;
