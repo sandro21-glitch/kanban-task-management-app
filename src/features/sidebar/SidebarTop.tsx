2ce5fb4716e1e63cd47ad6906cdd70ba147ffd2b
Sandro <vipsandro11@gmail.com>
2024-02-13 18:23:33 +0400

    style: enhance SidebarTop component with darkMode support

--- src/features/sidebar/SidebarTop.tsx
+++ src/features/sidebar/SidebarTop.tsx
@@ -5,6 +5,7 @@ import boardIcon from "/assets/board-icon.svg";
 const SidebarTop = () => {
   const dispatch = useAppDispatch();
   const boards = useAppSelector((store) => store.board.boards);
+  const darkMode = useAppSelector((store) => store.theme.darkMode);
   return (
     <div>
       <h2 className="uppercase text-[.8rem] text-lightGray font-bold tracking-widest mb-5 ml-5">
@@ -20,13 +21,17 @@ const SidebarTop = () => {
               className={`py-[1rem] ${
                 isActive
                   ? "bg-mediumPurple text-white"
+                  : darkMode
+                  ? "hover:bg-white"
                   : "hover:bg-sidebarHover"
               } overflow-hidden rounded-r-full flex items-center gap-5 cursor-pointer group`}
             >
               <img src={boardIcon} alt="board icon" className="ml-5 " />
               <h3
                 className={`text-[.9rem] font-bold ${
-                  isActive ? "text-white" : "text-lightGray group-hover:text-mediumPurple"
+                  isActive
+                    ? "text-white"
+                    : "text-lightGray group-hover:text-mediumPurple"
                 }  normal-case transition-colors ease-in duration-150`}
               >
                 {name}
