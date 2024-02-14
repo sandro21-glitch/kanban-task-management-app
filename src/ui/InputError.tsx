08c16be7a37df70d5e1a5a20ef75efd579ac5099
Sandro <vipsandro11@gmail.com>
2024-02-14 17:17:55 +0400

    feat: Enhance InputError component with customizable top position

--- src/ui/InputError.tsx
+++ src/ui/InputError.tsx
@@ -1,8 +1,16 @@
 type InputErrorTypes = {
   text: string;
+  top?: string;
 };
-const InputError = ({ text }: InputErrorTypes) => {
-  return <p className="text-red-500 absolute right-12 text-[.75rem]">{text}</p>;
+const InputError = ({ text, top }: InputErrorTypes) => {
+  return (
+    <p
+      style={{ top: `${top}rem` }}
+      className={`text-red-500 absolute right-12  text-[.75rem]`}
+    >
+      {text}
+    </p>
+  );
 };
 
 export default InputError;
