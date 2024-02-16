ae45e5a2f5e4c7241c0f8616bddbc2852633f83e
Sandro <vipsandro11@gmail.com>
2024-02-16 20:45:52 +0400

    refactor: Simplify BoardColumns component structure

--- src/features/board/BoardColumns.tsx
+++ src/features/board/BoardColumns.tsx
@@ -5,16 +5,15 @@ import { selectActiveBoard } from "./boardsSlice";
 const BoardColumns = () => {
   const activeBoard = useAppSelector(selectActiveBoard);
   if (!activeBoard) return null;
+
   const { boardTodos } = activeBoard;
+
   return (
-    <article>
-      <ul className="flex gap-7 text-lightGray">
-        {boardTodos.map((todo) => {
-          const { todoId } = todo;
-          return <TodoTasks key={todoId} todo={todo} />;
-        })}
-      </ul>
-    </article>
+    <ul className="flex gap-7 text-lightGray">
+      {boardTodos.map((todo) => (
+        <TodoTasks key={todo.todoId} todo={todo} />
+      ))}
+    </ul>
   );
 };
 
