b420a73e893e9f45fd1dbe33718a5996316868f3
Sandro <vipsandro11@gmail.com>
2024-02-17 18:28:08 +0400

    refactor: implement useOutsideClick hook in Sidebar component

--- src/features/sidebar/Sidebar.tsx
+++ src/features/sidebar/Sidebar.tsx
@@ -3,8 +3,9 @@ import SidebarTop from "./SidebarTop";
 import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
 import OpenSidebar from "./OpenSidebar";
 import SidebarBot from "./SidebarBot";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { setSidebar } from "../popups/popupSlice";
+import useOutsideClick from "../../hooks/useClickOutside";
 const Sidebar = () => {
   const { isSidebarOpen } = useAppSelector((store) => store.popup);
   const darkMode = useAppSelector((store) => store.theme.darkMode);
@@ -12,7 +13,7 @@ const Sidebar = () => {
   const [prevIsSmallScreen, setPrevIsSmallScreen] = useState(
     window.innerWidth <= 640
   );
-
+  const sidebarRef = useRef(null);
   useEffect(() => {
     const handleResize = () => {
       const newWindowWidth = window.innerWidth;
@@ -36,11 +37,21 @@ const Sidebar = () => {
     };
   }, [isSidebarOpen, prevIsSmallScreen, dispatch]);
 
+  const closeSidebar = () => {
+    if (isSidebarOpen) {
+      dispatch(setSidebar(false));
+    }
+  };
+
+  // Close sidebar when clicking outside
+  useOutsideClick(sidebarRef, closeSidebar);
+
   if (!isSidebarOpen) return <OpenSidebar />;
   return (
     <aside className="mt-10">
       <div className="bg-black opacity-50 fixed inset-0 w-full sm:hidden"></div>
       <div
+        ref={sidebarRef}
         className={`absolute left-0 right-0 top-20 w-[364px] sm:w-[264px] rounded-lg sm:rounded-none sm:static mx-auto sm:min-w-sidebarWidth ${
           darkMode ? "bg-darkMode border-r-mediumDark" : "bg-white"
         }
