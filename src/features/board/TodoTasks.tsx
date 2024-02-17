a628ed9199f5c1be2f4544b465d9def9c508d8fd
Sandro <vipsandro11@gmail.com>
2024-02-17 18:17:54 +0400

    feat: add dynamic background color to TodoTasks component

--- src/features/board/TodoTasks.tsx
+++ src/features/board/TodoTasks.tsx
@@ -1,6 +1,7 @@
+import { useState } from "react";
 import DroppableArea from "../../ui/DroppableArea";
 import TaskDetails from "./TaskDetails";
-
+import randomColor from "randomcolor";
 type TodoTasksTypes = {
   todo: {
     todoId: string;
@@ -17,16 +18,18 @@ type TodoTasksTypes = {
     }[];
   };
 };
-
 const TodoTasks = ({ todo }: TodoTasksTypes) => {
   const { todoName, todoTasks, todoId } = todo;
-
+  const [newTaskColor, setNewTaskColor] = useState<string>("");
+  if (!newTaskColor) {
+    setNewTaskColor(randomColor());
+  }
   return (
     <DroppableArea todoId={todoId}>
-      <li className="min-w-[17.5em] ">
+      <div className="min-w-[17.5em]">
         <div className="flex gap-2 items-center">
           <div
-            style={{ backgroundColor: "green" }}
+            style={{ backgroundColor: newTaskColor }}
             className="w-[15px] h-[15px] rounded-full"
           ></div>
           <div className="font-bold tracking-[.1rem] text-[.9rem]">
@@ -34,7 +37,7 @@ const TodoTasks = ({ todo }: TodoTasksTypes) => {
           </div>
         </div>
         <TaskDetails todoTasks={todoTasks} todoId={todoId} />
-      </li>
+      </div>
     </DroppableArea>
   );
 };
